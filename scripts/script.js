0c3af923878dec5ff1ff734e8ef31b438bd28c48
Pia <pia.koester@hey.com>
2023-10-05 08:46:40 +0200

    fix: problem I caused in selection buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,49 @@
+// Get the button with class "sweet"
+const sweetButton = document.querySelector('button.sweetButton');
+
+// Get the button with class "savory"
+const savoryButton = document.querySelector('button.savoryButton');
+
+// Get the button with class "allButton"
+const allButton = document.querySelector('button.allButton');
 
 // Get all elements with class "sweet"
-        const sweetDivs = document.getElementsByClassName('sweet');
-        
-        // Get the button with class "sweet"
-        const sweetButton = document.querySelector('button.sweetButton');
+const sweetDivs = document.querySelectorAll('.sweet');
 
-        // Add a click event listener to the "sweet" button
-        sweetButton.addEventListener('click', function() {
-            // Hide all divs with class "savory"
-            const savoryDivs = document.getElementsByClassName('savory');
-            savoryDivs.forEach(function(div) {
-                div.style.display = 'none';
-            });
+// Get all elements with class "savory"
+const savoryDivs = document.querySelectorAll('.savory');
 
-            // Show all divs with class "sweet"
-            sweetDivs.forEach(function(div) {
-                div.style.display = 'inline-block';
-            });
-        });
+// Add a click event listener to the "sweet" button
+sweetButton.addEventListener('click', function() {
+    // Hide all divs with class "savory"
+    savoryDivs.forEach(function(div) {
+        div.style.display = 'none';
+    });
 
-             // Get all elements with class "savory"
-             const savoryDivs = document.getElementsByClassName('savory');
-        
-             // Get the button with class "sweet"
-             const savoryButton = document.querySelector('button.savoryButton');
-     
-             // Add a click event listener to the "sweet" button
-             savoryButton.addEventListener('click', function() {
-                 // Hide all divs with class "savory"
-                 const sweetDivs = document.getElementsByClassName('sweet');
-                 sweetDivs.forEach(function(div) {
-                     div.style.display = 'none';
-                 });
-     
-                 // Show all divs with class "sweet"
-                 savoryDivs.forEach(function(div) {
-                     div.style.display = 'inline-block';
-                 });
-             });
+    // Show all divs with class "sweet"
+    sweetDivs.forEach(function(div) {
+        div.style.display = 'inline-block';
+    });
+});
 
-                     // Get the button with class "allButton"
-        const allButton = document.querySelector('button.allButton');
+// Add a click event listener to the "savory" button
+savoryButton.addEventListener('click', function() {
+    // Hide all divs with class "sweet"
+    sweetDivs.forEach(function(div) {
+        div.style.display = 'none';
+    });
 
+    // Show all divs with class "savory"
+    savoryDivs.forEach(function(div) {
+        div.style.display = 'inline-block';
+    });
+});
 
-        allButton.addEventListener('click', function() {
-            // Show all cards
-            const allCards = document.getElementsByClassName('.card');
-            allCards.forEach(function(div) {
-                div.style.display = 'inline-block';
-            });
-        });
+// Add a click event listener to the "all" button
+allButton.addEventListener('click', function() {
+    // Show all cards
+    const allCards = document.querySelectorAll('.card');
+    allCards.forEach(function(div) {
+        div.style.display = 'inline-block';
+    });
+});
